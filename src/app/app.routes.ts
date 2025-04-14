5edca7ce7071683755abe81560c0fe4d84a9e958
Syed Rafiul Kabir <rafiulkabir01.rucse@gmail.com>
2025-04-14 20:30:56 +0600

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'invoice', pathMatch: 'full' },
+    { path: 'invoice', component: InvoiceComponent }
+  ];
